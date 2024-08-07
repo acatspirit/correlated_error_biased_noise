3e200383c96d79cc3e9095045a4743de373d4703
acatspirit <acatspirit@gmail.com>
2024-08-07 09:33:26 -0400

    try again to get a result

--- plot_csv.py
+++ plot_csv.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-l = 5
+l = 6
 num_shots = 500000
-eta = 4.26
+eta = 5.89
 d_list = [11,13,15,17,19]
-p_list = np.linspace(0.01, 0.5, 500)
+p_list = np.linspace(0.01, 0.5, 250)
 prob_scale = [2*0.5/(1+eta), (1+2*eta)/(2*(1+eta))]
 ind_d = {1:'x', 2:'z', 3:'corr_z', 4:'total'}
 
