517ddc93c2cb17fc65ee55a3bcd25d52b8c703e8
acatspirit <acatspirit@gmail.com>
2024-08-09 16:32:41 -0400

    more eta fixes

--- plot_csv.py
+++ plot_csv.py
@@ -4,10 +4,10 @@ import glob
 import matplotlib.pyplot as plt
 import numpy as np
 from compass_code_correlated_error import concat_csv
-
+from datetime import datetime
 
 csv_file = 'corr_err_data.csv'
-concat_csv('corr_err_data/', csv_file)
+# concat_csv('corr_err_data/', csv_file)
 df = pd.read_csv(csv_file)
 
 
@@ -19,7 +19,10 @@ curr_num_shots = 10000
 prob_scale = {'x': 2*0.5/(1+curr_eta), 'z': (1+2*curr_eta)/(2*(1+curr_eta)), 'corr_z': 1, 'total':1}
 
 # Filter the DataFrame based on the input parameters
-filtered_df = df[(df['l'] == curr_l) & (df['eta'] == curr_eta) & (df['num_shots'] == curr_num_shots)]
+filtered_df = df[(df['l'] == curr_l) & (df['eta'] == curr_eta) & (df['num_shots'] == curr_num_shots) 
+                # & (df['time_stamp'].apply(lambda x: x[0:10]) == datetime.today().date())
+                 ]
+
 
 # Get unique error types and unique d values
 error_types = filtered_df['error_type'].unique()
@@ -37,7 +40,7 @@ for i, error_type in enumerate(error_types):
     # Plot each d value
     for d in d_values:
         d_df = error_type_df[error_type_df['d'] == d]
-        ax.plot(d_df['p']*prob_scale[error_type], d_df['num_log_errors'], label=f'd={d}')
+        ax.scatter(d_df['p']*prob_scale[error_type], d_df['num_log_errors'], label=f'd={d}')
     
     ax.set_title(f'Error Type: {error_type}')
     ax.set_xlabel('p')
