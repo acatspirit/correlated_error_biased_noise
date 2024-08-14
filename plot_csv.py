9c32e19c226fdd406f169db9c584c94b1c0bf812
acatspirit <acatspirit@gmail.com>
2024-08-14 11:50:56 -0400

    update for l4

--- plot_csv.py
+++ plot_csv.py
@@ -12,8 +12,8 @@ df = pd.read_csv(csv_file)
 
 
 # Input parameters
-curr_l = 5
-curr_eta = 4.26
+curr_l = 4
+curr_eta = 3
 curr_num_shots = 10000
 
 prob_scale = {'x': 2*0.5/(1+curr_eta), 'z': (1+2*curr_eta)/(2*(1+curr_eta)), 'corr_z': 1, 'total':1}
