aa0a3da4162ddf6e35c357f1dcc43fc0728a7aae
Arianna Meinking <am1155@dcc-login-04.oit.duke.edu>
2024-12-02 13:13:19 -0500

    l=5 x bias eta 0.01 data

--- corr_err_data.sh
+++ corr_err_data.sh
@@ -2,13 +2,13 @@
 #SBATCH -e slurm_%A_%a.err               # Error file location
 #SBATCH -o slurm_%A_%a.out 
 #SBATCH -p brownlab-gpu,common     # Show who you are and get priority
-#SBATCH --array=1-10                # How many jobs do you have (the int variable $SLURM_ARRAY_TASK_ID
+#SBATCH --array=1-100                # How many jobs do you have (the int variable $SLURM_ARRAY_TASK_ID
 #SBATCH -c 1
 #SBATCH --mail-type=END        
 #SBATCH --mail-user=am1155@duke.edu       # It will send you an email when the job is finishe$
 #SBATCH --mem=10G                # Memory, keep it as 10G
-#SBATCH --job-name=corr_err_l5_eta0.01_1000_data.out               # How is your output is called, you n$
-#SBATCH --job-name=x_corr_err_l5_eta0.01_1000_data.out               # How is your output is called, you n$
+#SBATCH --job-name=corr_err_l5_eta0.01_10000_data.out               # How is your output is called, you n$
+#SBATCH --job-name=x_corr_err_l5_eta0.01_10000_data.out               # How is your output is called, you n$
 #readarray -t PARAMS_ARRAY < params.txt
 #PARAMS=${PARAMS_ARRAY[(($SLURM_ARRAY_TASK_ID - 1))]}
 python3 compass_code_correlated_error.py 
