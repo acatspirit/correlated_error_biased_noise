81451706fd910256146fc744346de28540c956e7
acatspirit <acatspirit@gmail.com>
2024-12-03 16:30:07 -0800

    l3 eta 1 graphed, l3 eta 0.01 queued

--- corr_err_data.sh
+++ corr_err_data.sh
@@ -7,8 +7,8 @@
 #SBATCH --mail-type=END        
 #SBATCH --mail-user=am1155@duke.edu       # It will send you an email when the job is finishe$
 #SBATCH --mem=10G                # Memory, keep it as 10G
-#SBATCH --job-name=corr_err_l3_eta1_10000_data.out               # How is your output is called, you n$
-#SBATCH --job-name=x_corr_err_l3_eta1_10000_data.out               # How is your output is called, you n$
+#SBATCH --job-name=corr_err_l3_eta0.01_10000_data.out               # How is your output is called, you n$
+#SBATCH --job-name=x_corr_err_l3_eta0.01_10000_data.out               # How is your output is called, you n$
 #readarray -t PARAMS_ARRAY < params.txt
 #PARAMS=${PARAMS_ARRAY[(($SLURM_ARRAY_TASK_ID - 1))]}
 python3 compass_code_correlated_error.py 
