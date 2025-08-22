27852faacff43c39b59f70c5d0e6dfa289e287e6
acatspirit <acatspirit@gmail.com>
2025-08-22 15:46:34 -0400

    made some changes to arr size (not sure what is allowed) and fixed ind in l,eta,type iteration

--- corr_err_data.sh
+++ corr_err_data.sh
@@ -2,7 +2,7 @@
 #SBATCH -e slurm_%A_%a.err               # Error file location
 #SBATCH -o slurm_%A_%a.out 
 #SBATCH -p common     # Show who you are and get priority
-#SBATCH --array=0-89999                # How many jobs do you have (the int variable $SLURM_ARRAY_TASK_ID)
+#SBATCH --array=0-999                # How many jobs do you have (the int variable $SLURM_ARRAY_TASK_ID)
 #SBATCH -c 1
 #SBATCH --mail-type=END        
 #SBATCH --mail-user=am1155@duke.edu       # It will send you an email when the job is finishe$
