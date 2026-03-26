0c1cbe28a3aa5a9adbe247f8843e8d83bf698fee
acatspirit <acatspirit@gmail.com>
2026-03-26 11:51:12 -0400

    change what script we run in cutoff_simulation.sh

--- run_cutoff_simulation.sh
+++ run_cutoff_simulation.sh
@@ -4,8 +4,8 @@
 #SBATCH --array=0-63                  # 4*d × 4*p × 4*eta = 120 jobs
 #SBATCH --mem=4G                       # Memory per job
 #SBATCH --time=48:00:00                # Time limit
-#SBATCH -o logs/ler_%A_%a.out          # STDOUT
 #SBATCH -e logs/ler_%A_%a.err          # STDERR
+#SBATCH -o logs/ler_%A_%a.out          # STDOUT
 #SBATCH --mail-type=END,FAIL           # Email notifications
 
 
@@ -19,4 +19,4 @@ mkdir -p results
 # Run your script
 # -------------------------
 
-python run_ler_cutoff.py
+python cutoff_simulation.py
