367ac950569f24c837da8da9f51bd8f9371d3f46
acatspirit <acatspirit@gmail.com>
2026-04-20 12:26:09 -0400

    get the ler below threshold more more shots

---
+++ ler_download.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+
+scp "dcc:/hpc/group/brownlab/am1155/correlated_error_biased_noise/cluster_ler_chunks/*.csv" "/Users/ariannameinking/Documents/Brown_Research/correlated_error_biased_noise/cluster_ler_chunks/".
--- submit_ler_chunks.sh
+++ submit_ler_chunks.sh
@@ -2,9 +2,9 @@
 #SBATCH --job-name=ler_chunks
 #SBATCH --partition=common,gpu-common,scavenger,scavenger-gpu
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=3G
+#SBATCH --mem=1G
 #SBATCH --time=24:00:00
-#SBATCH --array=0-899
+#SBATCH --array=0-8999
 #SBATCH --output=logs/ler_%A_%a.out
 #SBATCH --error=logs/ler_%A_%a.err
 
@@ -13,8 +13,8 @@ mkdir -p cluster_ler_chunks
 
 python run_ler_chunk.py \
     --task_id ${SLURM_ARRAY_TASK_ID} \
-    --shots_per_chunk 1000 \
-    --num_chunks 100 \
+    --shots_per_chunk 10000 \
+    --num_chunks 1000 \
     --outdir cluster_ler_chunks \
     --l 3 \
     --basis X \
